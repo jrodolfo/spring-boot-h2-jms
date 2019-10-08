84fe6c2d39d7a3b201a87e236819ee05d8d755d9
Rod Oliveira <roliveir@ca.ibm.com>
2019-10-08 09:02:53 -0300

    adding comment to Topic

--- src/main/java/com/jrodolfo/eventhandler/model/Topic.java
+++ src/main/java/com/jrodolfo/eventhandler/model/Topic.java
@@ -18,6 +18,7 @@ public enum Topic {
     // We cannot use the toString() everywhere... If we try doing that in the following code, it does not work:
     //  @JmsListener(destination = Topic.TOPIC_01.toString(), <=== "Attribute value must be constant"
     // Therefore we define this Constants class to produce constant values
+    // so that we can use like that: Topic.Constants.TOPIC_01_VALUE
     public static class Constants {
         public static final String TOPIC_01_VALUE = "topic_01";
         public static final String TOPIC_02_VALUE = "topic_02";
